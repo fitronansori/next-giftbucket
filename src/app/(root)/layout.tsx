3e4d8737959d8c8b10d692878665543b66cf6130
fitronansori <fitronansori7@gmail.com>
2025-05-30 11:04:49 +0800

    commit

---
+++ src/app/(root)/layout.tsx
@@ -0,0 +1,13 @@
+import Footer from "@/components/layouts/Footer";
+import Header from "@/components/layouts/Header";
+
+const HomeLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </div>
+  );
+};
+export default HomeLayout;
---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+const HomePage = () => {
+  return (
+    <div className="container">
+      <Button>Click Me</Button>
+    </div>
+  );
+};
+export default HomePage;
