63cf299a9e2bae950fe2a0b6abbd6ecddfa71925
fitronansori <fitronansori7@gmail.com>
2025-05-31 08:57:38 +0800

    feat: add subClassName prop to Title component for customizable subtitle styling

--- src/components/common/Title.tsx
+++ src/components/common/Title.tsx
@@ -4,15 +4,23 @@ type TitleProps = {
   title?: string;
   subtitle?: string;
   className?: string;
+  subClassName?: string;
 };
 
-const Title = ({ title, subtitle, className }: TitleProps) => {
+const Title = ({ title, subtitle, className, subClassName }: TitleProps) => {
   return (
     <div className={cn("text-center space-y-2", className)}>
       <h3 className="text-2xl md:text-4xl font-bold">{title}</h3>
 
       {subtitle && (
-        <p className="text-sm md:text-base text-muted-foreground">{subtitle}</p>
+        <p
+          className={cn(
+            "text-sm md:text-base text-muted-foreground",
+            subClassName
+          )}
+        >
+          {subtitle}
+        </p>
       )}
     </div>
   );
