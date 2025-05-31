9c007eccd1d44b31af0f96424bfff8ca10b91a6d
fitronansori <fitronansori7@gmail.com>
2025-05-31 10:12:02 +0800

    feat: add Prices component to HomePage for displaying pricing information

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,5 +1,6 @@
 import Category from "@/components/layouts/Category";
 import Hero from "@/components/layouts/Hero";
+import Prices from "@/components/layouts/Prices";
 import Product from "@/components/layouts/Product";
 
 const HomePage = () => {
@@ -8,6 +9,7 @@ const HomePage = () => {
       <Hero />
       <Category />
       <Product />
+      <Prices />
     </>
   );
 };
